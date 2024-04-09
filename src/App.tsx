2953b709047dc60bad452070412778366cc102a0
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-09 18:12:39 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import Person, { Country } from "./Person";
 
 function App() {
   return (
     <div className="App">
-    
+      <Person
+        name="Jackson"
+        age={25}
+        email="ded@ded.com"
+        isMarried={true}
+        country= {Country.Canada}
+        friends={["Jessica", "Jhenny", "Jhonson"]}
+      />
     </div>
   );
 }
---
+++ src/Person.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface Props {
+    name: string;
+    age: number;
+    email: string;
+    isMarried: boolean;
+    country: Country;
+    friends: string[]
+}
+
+export enum Country {
+    Brazil= 'Brazil',
+    France= 'France',
+    Canada=' Canada'
+}
+
+
+const Person = (props: Props) => {
+    const [email, setEmail] = useState<string>('')
+
+    const handleChange =(eve:any)=>{
+        setEmail(eve.target.value)
+    }
+  return (
+    <div>
+        <p>Name: {props.name}</p>
+        <p>Age: {props.age}</p>
+        <p>Email:{email}</p>
+        <input type="email" onChange={handleChange}/>
+        <p>Marital Status: {props.isMarried ? 'Married':'Unmarried'}</p>
+        <p>Birth Place: {props.country}</p>
+        <p>Friends: {props.friends.map((friend)=><p>{friend}</p>)}</p>
+    </div>
+  )
+}
+
+export default Person
