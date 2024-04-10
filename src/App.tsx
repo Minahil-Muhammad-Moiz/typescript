4cce944d2ab586e6f57ba837d8e6ae554436fa26
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-11 00:57:45 +0500

    set InputField

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import InputField from "./Components/InputField";
 
 const App: React.FC =()=> {
+  const[todo, setTodo]= useState<string>("")
+
   return (
     <div className="w-screen h-screen flex flex-col text-center bg-red-400">
       <span className="uppercase text-white text-4xl z-10 my-8 md:my-10">Taskyfies</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,8 +1,16 @@
-const InputField = () => {
+import React from "react";
+
+interface Prop {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField:React.FC<Prop> = (props) => {
   return (
     <form className="flex justify-center items-center mx-auto relative md:w-[80%] w-[60%]">
-        <input type="input" placeholder="Enter a task" className="w-full rounded-[50px] px-8 py-5 text-2xl border-none transition shadow-inner shadow-black focus:outline-none focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.3)]"/>
+        <input type="input" placeholder="Enter a task" className="w-full rounded-[50px] px-8 py-5 text-2xl border-none transition-all shadow-inner shadow-black focus:outline-none focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.3)]" value={props.todo} onChange={(eve)=>(props.setTodo(eve.target.value))} />
         <button className="absolute right-0 w-14 h-11 bg-red-400 rounded-full m-3 border-none text-white text-2xl shadow-[0_0_6px_black] hover:bg-red-500  active:scale-[0.8] transition-all">Add</button>
+        
     </form>
   )
 }
